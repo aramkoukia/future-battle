e83739cdf5dbf2debd7910d3aed63078491dc741
Aram Koukia <aramkoukia@gmail.com>
2017-03-03 12:24:00 -0800

    added Hazards and Game controller

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+
+    // Use this for
+    public float tumble;
+
+	void Start () {
+        GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * tumble;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
